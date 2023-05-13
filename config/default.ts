6b6c2c109dfeedccc4548b7483da78b89288bccd
det-api <digitalengineeringtech.api@gmail.com>
2023-05-13 10:12:22 +0630

    config_update

---
+++ config/default.ts
@@ -0,0 +1,10 @@
+export default {
+  port: 9000,
+  host: "localhost",
+  dbUrl: "mongodb://127.0.0.1:27017/fuel-station",
+  saltWorkFactor: 10,
+  secretKey: "suuhh",
+  mqttUrl: "mqtts://f98a3730d6c3454b8cbf7d6c4f13cb69.s1.eu.hivemq.cloud",
+  mqttUser: "lmo-12",
+  mqttPassword: "Asdffdsa-4580",
+};
